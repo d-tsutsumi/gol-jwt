41962ecc180eef4a166df912f2d91cd8470fa3be
大樹堤 <kani.mono56@gmail.com>
2022-04-05 22:46:08 +0900

    add script and userapi

--- ambassador/src/routes/routes.go
+++ ambassador/src/routes/routes.go
@@ -18,4 +18,8 @@ func SetUp(app *fiber.App) {
 	adminAuthenticated := admin.Use(middlewares.IsAuthenticated)
 	adminAuthenticated.Get("/user", controllers.User)
 	adminAuthenticated.Post("logout", controllers.Logout)
+	adminAuthenticated.Put("/users/info", controllers.UpdateInfo)
+	adminAuthenticated.Put("/users/password", controllers.UpdatePassword)
+	adminAuthenticated.Get("ambassadors", controllers.Ambassadors)
+
 }
